a88ab425fde7ebf2eac65aed42568891829a8a0b
Jonnie Bissell <jonniembissell@gmail.com>
2022-02-10 19:29:13 -0500

    changed element of footer

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 let header = document.querySelector('#page-header')
-header.getElementsByClassName.textAlign = "left"
+let dogImages = document.querySelectorAll('.dog-image')
+for (let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+    footer.style.borderStyle = 'solid'
+}
+header.style.textAlign = "left"
